ba12bffc538d105f0fc2fccb7b52d7fd3083c6f0
msoderquist <msoderquist@mastercontrol.com>
2022-06-04 16:06:36 -0600

    Updated test algorithm

--- source/main/java/com/avereon/acorncli/AcornChecker.java
+++ source/main/java/com/avereon/acorncli/AcornChecker.java
@@ -5,18 +5,28 @@ import lombok.CustomLog;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.*;
 import java.util.function.Consumer;
 
+/**
+ * The way this works is that the tests are as many times as possible in a small
+ * period of time called a sample. Multiple samples are taken and the samples
+ * with the smallest amount of jitter are added to an iteration.
+ * <p>
+ * multiple small periods of time, a step, and grouped into iterations. The step
+ * with the least amount of jitter is chosen to represent the iteration. Several
+ * iterations are run and the values of all iterations are averaged together.
+ * This process seems to provide the most consistent results across multiple
+ * runs.
+ */
 @CustomLog
 public class AcornChecker implements Callable<Long> {
 
-	private static final int ITERATION_LIMIT = 10;
+	private static final int SAMPLE_TIME = 100;
 
-	private static final int STEPS_PER_ITERATION = 10;
+	private static final int ITERATION_COUNT = 5;
 
-	private int step;
+	private static final int SAMPLES_PER_ITERATION = 10;
 
 	private int steps;
 
@@ -29,9 +39,9 @@ public class AcornChecker implements Callable<Long> {
 	}
 
 	public AcornChecker( Runnable... tests ) {
-		listeners = new CopyOnWriteArraySet<>();
 		this.tests = tests;
-		setTotal( this.steps = tests.length * ITERATION_LIMIT * STEPS_PER_ITERATION );
+		this.listeners = new CopyOnWriteArraySet<>();
+		setTotal( this.steps = tests.length * ITERATION_COUNT );
 	}
 
 	@Override
@@ -59,13 +69,29 @@ public class AcornChecker implements Callable<Long> {
 		fireEvent( progress );
 	}
 
-	public long runTests( Runnable... tests ) throws InterruptedException {
-		this.step = 0;
-
+	public long runTests( Runnable... tests ) throws ExecutionException, InterruptedException {
 		long count = 0;
-		for( Runnable test : tests ) {
-			count += runTest( new Counter( test ) ).getAvg();
-		}
+
+		final int coreCount = getCoreCount();
+		ExecutorService executor = Executors.newFixedThreadPool( coreCount );
+
+//		// Start all the work
+//		Set<Future<Statistics>> futures = new HashSet<>();
+//		for( int coreIndex = 0; coreIndex < coreCount; coreIndex++ ) {
+//			for( Runnable test : tests ) {
+//				futures.add( executor.submit( () -> runTest( new Counter( test ) ) ) );
+//			}
+//		}
+//
+//		// Wait for all the work
+//		for( Future<Statistics> future : futures ) {
+//			count += future.get().getAvg();
+//		}
+
+		// Single thread
+				for( Runnable test : tests ) {
+					count += runTest( new Counter( test ) ).getAvg();
+				}
 
 		return count / tests.length / 100;
 	}
@@ -78,36 +104,32 @@ public class AcornChecker implements Callable<Long> {
 		listeners.remove( runnable );
 	}
 
+	private int count;
+
+	private Statistics runTest( Counter counter ) throws InterruptedException {
+		Statistics best = null;
+
+		for( int iterationIndex = 0; iterationIndex < ITERATION_COUNT; iterationIndex++ ) {
+			Statistics stats = new Statistics( SAMPLES_PER_ITERATION );
+			for( int sampleIndex = 0; sampleIndex < SAMPLES_PER_ITERATION; sampleIndex++ ) {
+				counter.runFor( SAMPLE_TIME );
+				stats.add( counter.getCount(), counter.getTime() );
+				count++;
+			}
+			stats.process();
+			if( best == null || stats.getJitter() < best.getJitter() ) best = stats;
+
+			// This logic takes into consideration how many cores are being used
+			if( count % getCoreCount() == 0 ) setProgress( count / getCoreCount() );
+		}
+
+		return best;
+	}
+
 	private void fireEvent( int step ) {
 		for( Consumer<Integer> listener : new HashSet<>( listeners ) ) {
 			listener.accept( step );
 		}
-	}
-
-	private Statistics runTest( Counter counter ) throws InterruptedException {
-		int time = 100;
-		int valueCount = 10;
-		int iterationLimit = ITERATION_LIMIT * STEPS_PER_ITERATION;
-		int iterationCount = 0;
-
-		Statistics bestStats = null;
-		int count = 0;
-		Statistics stats = new Statistics( valueCount );
-		do {
-			counter.runFor( time );
-			stats.add( counter.getCount(), counter.getTime() );
-			count++;
-			if( count >= valueCount ) {
-				stats.process();
-				if( bestStats == null || stats.getJitter() < bestStats.getJitter() ) bestStats = stats;
-				count = 0;
-				stats = new Statistics( valueCount );
-			}
-			setProgress( ++step );
-			iterationCount++;
-		} while( iterationCount < iterationLimit );
-
-		return bestStats;
 	}
 
 }
--- source/main/java/com/avereon/acorncli/Program.java
+++ source/main/java/com/avereon/acorncli/Program.java
@@ -24,19 +24,16 @@ public class Program {
 	public void run( String[] commands ) {
 		printHeader( card );
 
-		int scale = 10;
-		AcornChecker checker = new AcornChecker( new LoadTest() );
+		AcornChecker checker = new AcornChecker( new LoadTest(), new LoadTest() );
 		try {
-			int steps = checker.getStepCount() / scale;
+			int steps = checker.getStepCount();
 			System.out.println( "|" + "=".repeat( steps ) + "|" );
 			System.out.print( "|" );
-			checker.addListener( ( p ) -> {
-				if( p % scale == 0 ) System.out.print( '-' );
-			} );
+			checker.addListener( ( p ) -> System.out.print( '-' ) );
 			long score = checker.call();
 			System.out.println( "|" );
-			System.out.println( "Squirrel count: " + checker.getCoreCount() );
 			System.out.println( "Acorn score:    " + score );
+			System.out.println( "Squirrel count: " + checker.getCoreCount() );
 		} catch( Throwable throwable ) {
 			throwable.printStackTrace( System.err );
 		}
